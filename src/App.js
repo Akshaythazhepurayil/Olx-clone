0f77bb09e01beb6813c68cb76aa2ea248f24dfd6
Akshay T P <akshaythazhepurayil@gmail.com>
2022-10-03 23:03:19 +0530

    Header portion commpleated

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useContext, useEffect } from 'react';
+import { BrowserRouter as Router,Route } from 'react-router-dom'
+
+import Signup from './Pages/Signup'
+import Login from './Pages/Login'
+import './App.css';
+import {AuthContext, FirebaseContext} from './store/FirebaseContext'
+import Home from './Pages/Home';
+
+
+/**
+ * ?  =====Import Components=====
+ */
+
+
+function App() {
+  const {setUser} = useContext(AuthContext)
+  const {firebase} = useContext(FirebaseContext)
+  useEffect(()=>{
+    firebase.auth().onAuthStateChanged((user)=>{
+      setUser(user)
+    })
+    
+  })
+  return (
+    <div>
+      <Router>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/signup'>
+          <Signup />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+import 'firebase/auth'
+import 'firebase/firebase-firestore'
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBA_Q3LiUeC8Pp1D_TMP_cxf6uf_hTE7gE",
+  authDomain: "olx-clone-6374d.firebaseapp.com",
+  projectId: "olx-clone-6374d",
+  storageBucket: "olx-clone-6374d.appspot.com",
+  messagingSenderId: "297791547225",
+  appId: "1:297791547225:web:0d44fb32cff0efd20b4e48",
+  measurementId: "G-MEF1P8PCVJ"
+};
+
+    export default firebase.initializeApp(firebaseConfig)
